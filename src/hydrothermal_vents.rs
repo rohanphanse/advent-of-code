a220991a51218c978da7d9a070e00ca8bc248c53
Rohan Phanse <58747665+rohanphanse@users.noreply.github.com>
2022-07-24 16:23:43 +0000

    5.2 count line overlaps with diagonals

--- src/hydrothermal_vents.rs
+++ src/hydrothermal_vents.rs
@@ -6,6 +6,7 @@ use std::cmp::{ min, max };
 pub fn test() {
     println!("Day 5:");
     println!("Line overlaps: {}", count_line_overlaps());
+    println!("Line overlaps with diagonals: {}", count_line_overlaps_with_diagonals());
 }
 
 fn get_vent_data() -> Vec<((i32, i32), (i32, i32))>  {
@@ -82,4 +83,61 @@ fn _print_ocean_floor(ocean_floor: &Vec<Vec<i32>>) {
         }
         println!();
     }
+}
+
+fn count_line_overlaps_with_diagonals() -> i32 {
+    let vent_data = get_vent_data();
+    // Determine largest x and y values
+    let mut largest_x = 0;
+    let mut largest_y = 0;
+    for ((x1, y1), (x2, y2)) in &vent_data {
+        if *x1 > largest_x { largest_x = *x1; }
+        if *x2 > largest_x { largest_x = *x2; }
+        if *y1 > largest_y { largest_y = *y1; }
+        if *y2 > largest_y { largest_y = *y2; }
+    }
+    let mut ocean_floor: Vec<Vec<i32>> = Vec::with_capacity((largest_y + 1) as usize);
+    // Create ocean floor matrix
+    for r in 0..((largest_y + 1) as usize) {
+        ocean_floor.push(Vec::with_capacity((largest_x + 1) as usize));
+        for _ in 0..(largest_x + 1) {
+            ocean_floor[r].push(0);
+        }
+    }
+    // Add vent lines
+    for ((x1, y1), (x2, y2)) in &vent_data {
+        // Vertical lines
+        if *x1 == *x2 {
+            for y in (min(*y1, *y2) as usize)..=(max(*y1, *y2) as usize) {
+                ocean_floor[y][*x1 as usize] += 1;
+            }
+        // Horizontal lines
+        } else if *y1 == *y2 {
+            for x in (min(*x1, *x2) as usize)..=(max(*x1, *x2) as usize) {
+                ocean_floor[*y1 as usize][x] += 1;
+            }
+        // Diagonal lines
+        } else {
+            let x_start = min(*x1, *x2);
+            let x_end = max(*x1, *x2);
+            let y_start = if x_start == *x1 { *y1 } else { *y2 };
+            let y_end = if x_end == *x1 { *y1 } else { *y2 };
+            let y_direction = if (y_end - y_start) > 0 { 1 } else { -1 };
+            for i in 0..=(x_end - x_start) {
+                ocean_floor[(y_start + i * y_direction) as usize][(x_start + i) as usize] += 1;
+            }
+        }
+        
+    }
+    // print_ocean_floor(&ocean_floor);
+    // Count line overlaps (where value >= 2)
+    let mut overlaps = 0;
+    for r in 0..ocean_floor.len() {
+        for c in 0..ocean_floor[r].len() {
+            if ocean_floor[r][c] >= 2 {
+                overlaps += 1;
+            }
+        }
+    }
+    return overlaps;
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ mod binary_diagnostics;
 mod hydrothermal_vents;
 
 fn main() {
+    println!("Advent of Code 2021!");
     depth_increases::test();
     position_change::test();
     binary_diagnostics::test();
     hydrothermal_vents::test();
-    println!("Hello, world!");
 }
