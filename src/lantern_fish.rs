75ac2bf141c3942bf22a84b3d94563c54be13f6a
Rohan Phanse <58747665+rohanphanse@users.noreply.github.com>
2022-07-25 06:42:55 +0000

    Solve 6.2

--- src/lantern_fish.rs
+++ src/lantern_fish.rs
@@ -4,7 +4,7 @@ use std::fs;
 
 pub fn test() {
     println!("Day 6:");
-    println!("Lanternfish population after 80 days: {}", simulate(80));
+    println!("Lanternfish population after 80 days: {}", simulate_efficiently(80));
     println!("# of lanternfish after 256 days: {}", simulate_efficiently(256));
 }
 
@@ -16,7 +16,7 @@ fn get_initial_population() -> Vec<i32> {
     return numbers;
 }
 
-fn simulate(days: i32) -> i32 {
+fn _simulate(days: i32) -> i32 {
     let mut lantern_fish = get_initial_population();
     for _ in 0..days {
         for i in 0..lantern_fish.len() {
@@ -31,7 +31,16 @@ fn simulate(days: i32) -> i32 {
     return lantern_fish.len() as i32;
 }
 
-fn simulate_efficiently(days: i128) -> i32 {
-    let mut lantern_fish = get_initial_population();
-    return 0;
+fn simulate_efficiently(days: i32) -> i128 {
+    let initial_population = get_initial_population();
+    let mut lantern_fish: Vec<i128> = vec![0 ; 9];
+    for p in initial_population {
+        lantern_fish[p as usize] += 1;
+    }
+    for _ in 0..days {
+        // Shifts elements to left, cycles first element to end
+        lantern_fish.rotate_left(1);
+        lantern_fish[6] += lantern_fish[8];
+    }
+    return lantern_fish.iter().sum();
 }
